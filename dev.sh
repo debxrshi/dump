c50a603f9080c9974db5bc1233a37b8903d8cf8c
Debarshi Das <40909973+debxrshi@users.noreply.github.com>
2023-08-04 20:04:22 +0530

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -7,29 +7,19 @@ if [[ $EUID -ne 0 ]]; then
 fi
 git clone https://github.com/optiv/ScareCrow.git
 cd ScareCrow
-if go version 2>/dev/null; then
-	echo go install found, downloading packages
-	go get github.com/fatih/color
-	go get github.com/yeka/zip
-	go get github.com/josephspurrier/goversioninfo
-	go get github.com/Binject/debug/pe
-	go get github.com/awgh/rawreader
-	go get github.com/mattn/go-isatty@latest
-else
-	echo Installing go, requires sudo
-	sudo apt install -y golang
-	echo go installed, installing packages
-	go get github.com/fatih/color
-	go get github.com/yeka/zip
-	go get github.com/josephspurrier/goversioninfo
-	go get github.com/Binject/debug/pe
-	go get github.com/awgh/rawreader
-	go get github.com/mattn/go-isatty@latest
-fi
+echo Installing go, requires sudo
+apt install -y golang
+echo go installed, installing packages
+go get github.com/fatih/color
+go get github.com/yeka/zip
+go get github.com/josephspurrier/goversioninfo
+go get github.com/Binject/debug/pe
+go get github.com/awgh/rawreader
+go get github.com/mattn/go-isatty@latest
 echo Installing dependencies: OpenSSL, OSSLsigncode, MinGW-w64
-sudo apt install -y openssl
-sudo apt install -y osslsigncode
-sudo apt install -y mingw-w64
+apt install -y openssl
+apt install -y osslsigncode
+apt install -y mingw-w64
 echo Dependencies installed, building ScareCrow
 go build ScareCrow.go
 echo Adding ScareCrow to PATH
