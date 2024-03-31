69e7629a45231fbdd9c18c4e9ed4116a47932458
debxrshi <dasdebarshi19@gmail.com>
2024-03-31 18:55:23 +0530

    added skid.sh

---
+++ skid.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# For the skids by a skid(?) lol
+
+
+echo "
+ @@@@@@   @@@  @@@  @@@  @@@@@@@         @@@@@@   @@@  @@@ 
+@@@@@@@   @@@  @@@  @@@  @@@@@@@@       @@@@@@@   @@@  @@@ 
+!@@       @@!  !@@  @@!  @@!  @@@       !@@       @@!  @@@ 
+!@!       !@!  @!!  !@!  !@!  @!@       !@!       !@!  @!@ 
+!!@@!!    @!@@!@!   !!@  @!@  !@!       !!@@!!    @!@!@!@! 
+ !!@!!!   !!@!!!    !!!  !@!  !!!        !!@!!!   !!!@!!!! 
+     !:!  !!: :!!   !!:  !!:  !!!            !:!  !!:  !!! 
+    !:!   :!:  !:!  :!:  :!:  !:!  :!:      !:!   :!:  !:! 
+:::: ::    ::  :::   ::   :::: ::  :::  :::: ::   ::   ::: 
+:: : :     :   :::  :    :: :  :   :::  :: : :     :   : : 
+
+                                      
+                                         -- @debxrshi
+"
+
+# usage
+
+if [ "$#" -eq 0 ] || [ -z "$1" ]
+then
+    echo "usage: skid.sh target.com"
+    exit 1
+else
+    target=$1
+fi
+
+if [ -n "$target" ]
+then
+    echo -e "...Executing tools...\n"
+
+# initial probing
+
+    echo "[+] Probing target with httpx"
+
+    httpx -u "$target" -fr -sc -title -td -server -retries 3 -fc 404 -lc -t 500 
+
+# subfinder
+
+    echo -e "\n[+] Hunting subdomains with subfinder \n\n"
+    subfinder -d "$target" -o "${target}_subs"
+
+# probing subs
+
+    echo -e "\n Probing subdomains with httpx\n\n"
+    httpx -l "${target}_subs" -fr -sc -title -td -server -retries 3 -fc 404 -lc -t 500 -o "${target}_subs_httpx"
+
+# asnmap
+
+# dnsx
+
+# katana
+
+# uro
+
+# xsstrike
+
+# dalfox 
+
+fi
