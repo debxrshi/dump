3d979886e1d1d335df52f634cf07f2f235cde82e
debxrshi <dasdebarshi19@gmail.com>
2024-05-06 00:54:18 +0530

    refactoring duh

--- binex4binja.sh
+++ binex4binja.sh
@@ -2,27 +2,86 @@
 
 
 # Reference: https://gist.github.com/psifertex/31d9bc3167eca91e466ebaae4382521c
-# The script provided as reference is really good but doesn't work out of the box. Here's an edited version that's basically plug-n-play and also handles dependencies.
 
 
+echo -e "Build script for BinExport for Binja macOS stable/dev release by @debxrshi\n"
 
 
-echo "Build script for BinExport for Binja macOS ANY version by @debxrshi"
-echo -e "\nThis script assumes that Binary Ninja has been installed in default location. If that's not the case please edit it.\n\n"
+usage() {
+  echo -e "Usage:\n -dev: dev release.\n -stable: stable release."
+  exit 1
+}
 
-# Setting environment variables
+handle_deps(){
+    deps=("cmake","clang-format","ninja")
 
-$BN_PATH=
-$BN_HASH=$(sed 's/^.*\///' < $BN_PATH/api_REVISION.txt 2>/dev/null)
+    for dep in "${deps[@]}"; do
+        if type "$dep" &> /dev/null; then
+            echo -e "$dep exits\n"
+        else
+            echo -e "$dep not found\nInstalling $dep\n"
+            brew install $dep
+    fi
+done
+}
+
+set_hash(release){}
+
+if [ $# -ne 1 ]; then
+  usage
+fi
+
+# Configuration
+
+BN_PATH=`cat ~/Library/Application\ Support/Binary\ Ninja/lastrun`
+GIT_HASH=
+CORES=$(sysctl -n hw.logicalcpu)
+
+# Checking dependencies
+
+echo -e "[+] Checking dependencies\n"
+handle_deps
+
+# Confirming configuration
+
+echo -e "\nConfig\n"
+echo "HOME: $HOME"
+echo "BN_PATH: $BN_PATH"
+echo "GIT_HASH: $GIT_HASH"
+echo "CORES: $CORES"
+echo "BUILD: $1"
+
+read "press Enter to continue"
+
+# Git pull binexport 
+
+git pull https://github.com/google/binexport.git
 
 
-# Changing CMakeDeps to update hash
+# Setting hash location
+
+echo -w "[+] Updating git hash for $1"
 
 
-# Changing cmake.cc to add MACRO
+# Changing CMakeDeps to update hash and adding macro to cmake.cc
+
+echo -e "[+] Updating hash and inserting macro\n"
 
 
 # Building binexport
 
+echo -e "[+] Building BinExport may the force be with you"
+
 
 # Copying plugin to directory
+
+echo -e "[+] Build successful! Installing plugin now"
+
+echo -e "[+] Plugin already exits. Replacing it"
+
+
+# Performing cleaning
+
+echo -e "[+] Cleaning up"
+
+
