ab49018546cf419224363a4937871fca86102348
debxrshi <dasdebarshi19@gmail.com>
2024-05-06 01:44:32 +0530

    refactoring

--- binex4binja.sh
+++ binex4binja.sh
@@ -4,43 +4,38 @@
 # Reference: https://gist.github.com/psifertex/31d9bc3167eca91e466ebaae4382521c
 
 
-echo -e "Build script for BinExport for Binja macOS stable/dev release by @debxrshi\n"
+echo -e "Build script for BinExport for Binja macOS stable/dev release by @debxrshi\nThis script assumes you Binja is installed to its default location. Edit it if not.\n"
 
 
-usage() {
-  echo -e "Usage:\n -dev: dev release.\n -stable: stable release."
-  exit 1
-}
-
-handle_deps(){
-    deps=("cmake","clang-format","ninja")
-
-    for dep in "${deps[@]}"; do
-        if type "$dep" &> /dev/null; then
-            echo -e "$dep exits\n"
-        else
-            echo -e "$dep not found\nInstalling $dep\n"
-            brew install $dep
-    fi
-done
-}
-
-set_hash(release){}
 
 if [ $# -ne 1 ]; then
-  usage
+  echo -e "Usage:\n dev: dev release.\n stable: stable release."
+  exit 1
 fi
+
 
 # Configuration
 
-BN_PATH=`cat ~/Library/Application\ Support/Binary\ Ninja/lastrun`
+CHANNEL=$1
 GIT_HASH=
+PLUGIN_SRC=
+PLUGIN_DST=
 CORES=$(sysctl -n hw.logicalcpu)
 
 # Checking dependencies
 
 echo -e "[+] Checking dependencies\n"
-handle_deps
+
+deps=("cmake","clang-format","ninja")
+
+for dep in "${deps[@]}"; do
+  if type "$dep" &> /dev/null; then
+    echo -e "$dep exists\n"
+  else
+    echo -e "$dep not found\nInstalling $dep\n"
+    brew install $dep 
+  fi
+done
 
 # Confirming configuration
 
@@ -49,29 +44,28 @@ echo "HOME: $HOME"
 echo "BN_PATH: $BN_PATH"
 echo "GIT_HASH: $GIT_HASH"
 echo "CORES: $CORES"
-echo "BUILD: $1"
+echo "BUILD: $CHANNEL"
 
 read "press Enter to continue"
 
-# Git pull binexport 
+# Pull binexport 
 
-git pull https://github.com/google/binexport.git
-
-
-# Setting hash location
-
-echo -w "[+] Updating git hash for $1"
-
+git pull https://github.com/google/binexport.git $HOME
+cd $HOME/binexport
 
 # Changing CMakeDeps to update hash and adding macro to cmake.cc
 
 echo -e "[+] Updating hash and inserting macro\n"
 
 
+
 # Building binexport
 
-echo -e "[+] Building BinExport may the force be with you"
+echo -e "[+] Building BinExport may the force be with you\n"
 
+mkdir build && cd build
+cmake .. -G ninja -DBINEXPORT_BINARYNINJA_CHANNEL=$CHANNEL -DCMAKE_BUILD_TYPE=Release "-DCMAKE_INSTALL_PREFIX=$PWD" -DBINEXPORT_ENABLE_IDAPRO=OFF -DBINEXPORT_ENABLE_BINARYNINJA=ON
+cmake --build . --config Release -- "-j$CORES"
 
 # Copying plugin to directory
 
